09d4fc7a52cc052d2a2565b5f9d2342b329c2597
matteo-d-antonio <m.dantonio@elitesoftwarehouse.com>
2025-07-16 09:53:11 +0200

    inizio del progetto e aggiunta di tailwind

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    /*fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(response => setUsers(response))*/
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => setUsers(response.data))
+  }, []);
+
+  function addUser() {
+    /*fetch('https://jsonplaceholder.typicode.com/users', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({ name: `user ${Math.random()}` })
+    })
+      .then(response => response.json())
+      .then(response => setUsers(s => [...s, response]))*/
+    axios.post('https://jsonplaceholder.typicode.com/users', {
+      name: `user ${Math.random()}`
+    })
+      .then(response => setUsers(s => [...s, response.data]))
+  }
+
+  return (
+    <>
+      <button className="bg-red-500" onClick={addUser}>Aggiungi</button>
+      {
+        users.map(user => {
+          return <li key={user.id}> {user.name}</li>
+        
+        })
+      }
+    </>
+  )
+}
+
+export default App
